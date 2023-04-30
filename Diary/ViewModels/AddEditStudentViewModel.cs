3888731706163d29d6a0e699a8fbf2c7974e5cde
Robert Wolniewicz <robert_wol@wp.pl>
2023-04-30 16:06:04 +0200

    Add project files.

---
+++ Diary/ViewModels/AddEditStudentViewModel.cs
@@ -0,0 +1,117 @@
+﻿using Diary.Commands;
+using Diary.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Diary.ViewModels
+{
+    public class AddEditStudentViewModel : ViewModelBase
+    {
+        public AddEditStudentViewModel(Student student = null)
+        {
+            CloseCommand = new RelayCommand(Close);
+            ConfirmCommand = new RelayCommand(Confirm);
+
+            if (student == null)
+            {
+                Student = new Student();
+            }
+            else
+            {
+                Student = student;
+                IsUpdate = true;
+            }
+
+        }
+
+
+
+        public ICommand CloseCommand { get; set; }
+        public ICommand ConfirmCommand { get; set; }
+
+        Student _student;
+
+        public Student Student
+        {
+            get { return _student; }
+            set
+            {
+                _student = value;
+                OnPropertyChanged();
+            }
+        }
+
+        bool _isUpdate;
+
+        public bool IsUpdate
+        {
+            get { return _isUpdate; }
+            set
+            {
+                _isUpdate = value;
+                OnPropertyChanged();
+            }
+        }
+        private int _selectedGroupId;
+
+        public int SelectedGroupId
+        {
+            get { return _selectedGroupId; }
+            set
+            {
+                _selectedGroupId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        ObservableCollection<Group> _group;
+
+        public ObservableCollection<Group> Groups
+        {
+            get { return _group; }
+            set
+            {
+                _group = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private void Confirm(object obj)
+        {
+            if (!IsUpdate)
+                AddStudent();
+
+            else
+                UpdateStudent();
+
+            CloseWindow(obj as Window);
+        }
+
+        private void AddStudent()
+        {
+            // baza
+        }
+
+        private void UpdateStudent()
+        {
+            //baza
+        }
+
+        private void Close(object obj)
+        {
+            CloseWindow(obj as Window);
+        }
+
+        void CloseWindow(Window window)
+        {
+            window.Close();
+        }
+    }
+}
---
+++ Diary/ViewModels/MainViewModel.cs
@@ -0,0 +1,117 @@
+﻿using Diary.Commands;
+using Diary.Models;
+using Diary.Views;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Diary.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            AddStudentsCommand = new RelayCommand(AddEditStudents);
+            EditStudentsCommand = new RelayCommand(AddEditStudents, CanEditDeleteStudent);
+            DeleteStudentsCommand = new AsyncRelayCommand(DeleteStudents, CanEditDeleteStudent);
+            RefreshStudentsCommand = new RelayCommand(RefreshStudents);
+            RefreshDiary();
+        }
+
+        public ICommand RefreshStudentsCommand { get; set; }
+        public ICommand AddStudentsCommand { get; set; }
+        public ICommand EditStudentsCommand { get; set; }
+        public ICommand DeleteStudentsCommand { get; set; }
+
+        Student _selectedStudent;
+
+        public Student SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set
+            {
+                _selectedStudent = value;
+                OnPropertyChanged();
+            }
+        }
+        ObservableCollection<Student> _students;
+
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _selectedGroupId;
+
+        public int SelectedGroupId
+        {
+            get { return _selectedGroupId; }
+            set
+            {
+                _selectedGroupId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        ObservableCollection<Group> _group;
+
+        public ObservableCollection<Group> Groups
+        {
+            get { return _group; }
+            set
+            {
+                _group = value;
+                OnPropertyChanged();
+            }
+        }
+
+        void RefreshStudents(object obj)
+        {
+            RefreshDiary();
+        }
+        private async Task DeleteStudents(object obj)
+        {
+            var metroWindow = Application.Current.MainWindow as MetroWindow;
+            var dialog = await metroWindow.ShowMessageAsync
+                ("Usuwanie ucznia", $"Czy napewno chcesz usunąć ucznia{SelectedStudent.FirstName}{SelectedStudent.LastName}?",
+                MessageDialogStyle.AffirmativeAndNegative);
+
+            if (dialog != MessageDialogResult.Affirmative)
+            {
+                return;
+            }
+            //usuwanie
+            RefreshDiary();
+        }
+
+        private void AddEditStudents(object obj)
+        {
+            var addEditStudentWindow = new AddEditStudentView(obj as Student);
+            addEditStudentWindow.Closed += AddEditStudentWindow_Closed;
+            addEditStudentWindow.ShowDialog();
+        }
+
+        private void AddEditStudentWindow_Closed(object sender, EventArgs e)
+        {
+            RefreshDiary();
+        }
+
+        private bool CanEditDeleteStudent(object obj)
+        {
+            return SelectedStudent != null;
+        }
+        void RefreshDiary()
+        {
+
+        }
+    }
+}
